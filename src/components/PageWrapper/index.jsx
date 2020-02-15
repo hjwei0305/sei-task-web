62e81743ac21724a048cd48d708c797203df8d9b
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-02-15 16:32:28 +0800

    优化

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import cls from 'classnames';
+import { Spin, } from 'antd';
 import styles from './index.less';
 
 const PageWrapper = (props) => {
-  const { children, className, } = props;
+  const { children, className, loading=false } = props;
   return (
     <div className={cls(styles['sei-page-warpper'], className)}>
-      {children}
+      <Spin spinning={loading} wrapperClassName={cls("spin-wrapper")}>
+        {children}
+      </Spin>
     </div>
   )
 }
--- src/pages/Job/index.jsx
+++ src/pages/Job/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect, } from 'dva';
-import { Tabs, Spin } from 'antd';
+import { Tabs, } from 'antd';
 import { PageWrapper, } from '@/components';
 import BackJob from './componets/BackJob';
 import cls from 'classnames';
@@ -16,17 +16,15 @@ class Job extends React.PureComponent {
     const { loading, } = this.props;
 
     return (
-      <PageWrapper className={cls(styles['container-box'])}>
-        <Spin spinning={loading.global} wrapperClassName={cls("spin-wrapper")}>
-          <Tabs defaultActiveKey="1">
-            <TabPane tab="作业配置" key="1">
-              <BackJob />
-            </TabPane>
-            <TabPane tab="作业监控" key="2">
-              <ScheduleJob />
-            </TabPane>
-          </Tabs>
-        </Spin>
+      <PageWrapper className={cls(styles['container-box'])} loading={loading.global}>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="作业配置" key="1">
+            <BackJob />
+          </TabPane>
+          <TabPane tab="作业监控" key="2">
+            <ScheduleJob />
+          </TabPane>
+        </Tabs>
       </PageWrapper>
     );
   }
